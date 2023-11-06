340175801c807fcc43090263417b55d433d2cbdc
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2023-11-06 17:31:05 +0200

    Update Main.java
    I have modified the main class and added two functions: handleDecompression and decompressFile.

--- Main.java
+++ Main.java
@@ -1,41 +1,65 @@
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        while (true){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
             printMainMenu();
-            Scanner myReader = new Scanner(System.in);
-            int choice = myReader.nextInt();
 
-            System.out.println("Please enter the filename");
-            String filePath = myReader.next();
-            File file = new File(filePath);
+            try {
+                String choice = scanner.nextLine().toLowerCase();
 
-            System.out.println("Please enter the destination filename");
-            String destFilePath = myReader.next();
-
-            myReader.close();
-
-            if (choice == 1){
-                LZW.compressFile(filePath, destFilePath);
-            } else if (choice == 2){
-                LZW.decompressFile(filePath, destFilePath);
-            } else if (choice == 3) {
-                System.out.println("Thank you for using our program\n");
-                return;
-            } else {
-                System.out.println("Invalid option!");
+                switch (choice) {
+                    case "1", "compress" -> handleCompression(scanner);
+                    case "2", "decompress" -> handleDecompression(scanner);
+                    case "3", "exit" -> {
+                        System.out.println("Thank you for using LZW program\n");
+                        scanner.close(); // Close the scanner
+                        return;
+                    }
+                    default -> System.out.println("Invalid option! Please enter a valid choice.");
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("Error: File not found - " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("Error: IOException - " + e.getMessage());
             }
         }
     }
 
-        public static void printMainMenu() {
-        System.out.println("LZW Compression and Decompression Menu :)");
-        System.out.println("1. Compression");
-        System.out.println("2. Decompression");
-        System.out.println("3. exit.");
-        System.out.print("Choose an option (1, 2, or 3): ");
+    // Helper method to print the menu
+    private static void printMainMenu() {
+        System.out.println("Options:");
+        System.out.println("1. Compress file");
+        System.out.println("2. Decompress file");
+        System.out.println("3. Exit");
+        System.out.print("Enter your choice: ");
+    }
+
+    // Handle compression process
+    private static void handleCompression(Scanner scanner) throws IOException {
+        System.out.print("Please enter the input filename: ");
+        String inputFilePath = scanner.nextLine();
+
+        System.out.print("Please enter the destination filename: ");
+        String destFilePath = scanner.nextLine();
+
+        LZW.compressFile(inputFilePath, destFilePath);
+        System.out.println("Compression successful: " + inputFilePath + " -> " + destFilePath + "\n");
+    }
+
+    // Handle decompression process
+    private static void handleDecompression(Scanner scanner) throws IOException {
+        System.out.print("Please enter the input filename: ");
+        String inputFilePath = scanner.nextLine();
+
+        System.out.print("Please enter the destination filename: ");
+        String destFilePath = scanner.nextLine();
+
+        LZW.decompressFile(inputFilePath, destFilePath);
+        System.out.println("Decompression successful: " + inputFilePath + " -> " + destFilePath + "\n");
     }
 }

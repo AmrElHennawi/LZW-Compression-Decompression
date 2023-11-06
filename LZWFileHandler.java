028b8cfb5e36dd36ded2f5e974afeceb392f1364
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2023-11-06 12:06:01 +0200

    Update LZWFileHandler.java
    I have added two functions, 'writeCompressedFile' and 'readTxtFromFile,' to the LZWFileHandler class.

--- LZWFileHandler.java
+++ LZWFileHandler.java
@@ -1,13 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class LZWFileHandler {
-    public static ArrayList<Integer> readFile(String path){
 
-        // dummy return
-        return  new ArrayList<>();
+    public static String readTxtFromFile(String inputFileName) {
+        String output = "";
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                output += line + '\n';
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output;
     }
 
-    public static void writeFile(String path, ArrayList<Integer> encodedData){
+    public static void writeCompressedFile(String path, ArrayList<Integer> encodedData) {
+        try (PrintWriter outputFile = new PrintWriter(path)) {
+            for (Integer tag : encodedData) {
+                outputFile.println(tag);
+                outputFile.flush();
+            }
+        } catch (IOException e) {
+            System.out.println("Error while writing to the file: " + e.getMessage());
+        }
 
     }
 }

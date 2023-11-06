301ed4d1769f5704ce63bc9527868aca11c1c646
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2023-11-06 16:52:56 +0200

    Update TextFileHandler.java
    I have added the implementation of 'readFile' in the TextFileHandler class.

--- TextFileHandler.java
+++ TextFileHandler.java
@@ -1,15 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class TextFileHandler {
 
-    // TODO
-    public static String readFile(String path){
-        return "";
+    public static String readFile(String path) throws IOException {
+        String output = "";
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                output += line + '\n';
+            }
+        }
+        return output;
     }
 
     public static void writeFile(String path, String data) throws IOException {
-        try(FileWriter destFile = new FileWriter(path)){
+        try (FileWriter destFile = new FileWriter(path)) {
             destFile.write(data);
         }
 

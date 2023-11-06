83e0ed9281f9c3a7f7df5d7d8e18a57f89732c03
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2023-11-06 17:29:19 +0200

    Update LZWFileHandler.java
    I implemented the 'writeFile' function in the LZWFileHandler.

--- LZWFileHandler.java
+++ LZWFileHandler.java
@@ -7,48 +7,26 @@ public class LZWFileHandler {
     public static ArrayList<Integer> readFile(String path) throws FileNotFoundException {
         ArrayList<Integer> encodedData = new ArrayList<Integer>();
         File file = new File(path);
-        if (!file.exists()){
+        if (!file.exists()) {
             throw new FileNotFoundException();
         }
 
         Scanner myReader = new Scanner(file);
 
-        while (myReader.hasNextLine()){
+        while (myReader.hasNextLine()) {
             encodedData.add(Integer.valueOf(myReader.nextLine()));
         }
 
         myReader.close();
-
         return encodedData;
     }
 
-    // TODO
-    public static void writeFile(String path, ArrayList<Integer> encodedData){
-        // write an encoded data (integers) to a file separated by newlines
-
+    public static void writeFile(String path, ArrayList<Integer> encodedData) throws FileNotFoundException {
+        PrintWriter outputFile = new PrintWriter(path);
+        for (Integer tag : encodedData) {
+            outputFile.println(tag);
+            outputFile.flush();
+        }
+        outputFile.close();
     }
-
-//    public static String readTxtFromFile(String inputFileName) {
-//        String output = "";
-//        try (BufferedReader br = new BufferedReader(new FileReader(inputFileName))) {
-//            String line;
-//            while ((line = br.readLine()) != null) {
-//                output += line + '\n';
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return output;
-//    }
-//
-//    public static void writeCompressedFile(String path, ArrayList<Integer> encodedData) {
-//        try (PrintWriter outputFile = new PrintWriter(path)) {
-//            for (Integer tag : encodedData) {
-//                outputFile.println(tag);
-//                outputFile.flush();
-//            }
-//        } catch (IOException e) {
-//            System.out.println("Error while writing to the file: " + e.getMessage());
-//        }
-//    }
 }

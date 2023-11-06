57ed42ca3315960b55490d88fb963144b929d23e
Amr Khaled ElHennawi <103025432+AmrElhennawi@users.noreply.github.com>
2023-11-06 10:48:39 +0200

    Update LZW.java
    I have added the compression implementation in the LZW class

--- LZW.java
+++ LZW.java
@@ -2,22 +2,47 @@ import java.util.*;
 
 public class LZW {
 
-
     public static void main(String[] args) {
-
+        // Scanner in = new Scanner(System.in);
+        // System.out.print("Enter the string to compress: ");
+        // String str = in.nextLine();
+        // System.out.println("Compressed Output: " + compression(str));
     }
-    public static ArrayList<Integer> compression(String plainText){
+
+    public static ArrayList<Integer> compression(String plainText) {
         Map<String, Integer> dictionary = new HashMap<>();
         ArrayList<Integer> compressedOutput = new ArrayList<>();
+        int dictSize = 128;
+        String subText = "";
 
-        return new ArrayList<>();
+        for (int i = 0; i < dictSize; i++) {
+            dictionary.put(String.valueOf((char) i), i);
+        }
+
+        for (int i = 0; i < plainText.length(); i++) {
+            char currentChar = plainText.charAt(i);
+            String subTextPlusCurrent = subText + currentChar;
+
+            if (dictionary.containsKey(subTextPlusCurrent)) {
+                subText = subTextPlusCurrent;
+            } else {
+                compressedOutput.add(dictionary.get(subText));
+                dictionary.put(subTextPlusCurrent, dictSize++);
+                subText = String.valueOf(currentChar);
+            }
+        }
+
+        if (!subText.equals("")) {
+            compressedOutput.add(dictionary.get(subText));
+        }
+
+        return compressedOutput;
     }
 
-
-    public static String Decompression(ArrayList<Integer> encodedData){
+    public static String Decompression(ArrayList<Integer> encodedData) {
         Map<Integer, Character> dictionary = new HashMap<>();
 
-        for (int i = 0; i < 128; i++){
+        for (int i = 0; i < 128; i++) {
             dictionary.put(i, (char) i);
         }
 
